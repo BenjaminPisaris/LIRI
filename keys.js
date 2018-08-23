7891ef87c2da8e6f558aea380a3eace77d8cace2
Ben Pisaris <datcrisis@gmail.com>
2018-08-23 13:02:24 -0400

    added things

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log("loaded keys.js");
+
+//TO DO: find out how to get the key to work in here without a formatting error
+exports.spotify = {
+    id: process.env.SPOTID,
+    secret: process.env.SPOTISECRET
+};
---
+++ liri.js
@@ -0,0 +1,8 @@
+//set requirements
+var inquirer = require("inquirer");
+var request = require("request");
+var dotenv = require("dotenv");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+
+//dotENV config
