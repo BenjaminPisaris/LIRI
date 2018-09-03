4df8ac1503d1b46556ac586107fe744209dbf98b
Ben Pisaris <datcrisis@gmail.com>
2018-09-03 13:39:14 -0400

    finished

--- liri.js
+++ liri.js
@@ -3,6 +3,70 @@ var inquirer = require("inquirer");
 var request = require("request");
 var dotenv = require("dotenv");
 var fs = require("fs");
-var Spotify = require("node-spotify-api");
+var SpotifyWebApi = require("node-spotify-api");
 
 //dotENV config
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new SpotifyWebApi(keys.spotify);
+
+//INQUIRER
+function liri() {
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            name: "queryType",
+            message: "What can LIRI assist you with?",
+            choices: ["Spotify this song", "Movie Search", "Do what it says"]
+        }
+    ]).then(function (answer) {
+
+        console.log(answer.queryType);
+        if (answer.queryType == "Spotify this song") {
+            spotiSearch();
+        } else if (answer.queryType == "Movie Search") {
+            movieSearch();
+        } else if (answer.queryType === "Do what it says") {
+            itSays();
+
+        }
+    })
+}
+
+//FUNCTIONS
+function spotiSearch() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "songSearch",
+            message: "What song do you want to search for?",
+        }
+    ]).then(function (answer) {
+        spotify.search({ type: 'track', query: answer.songSearch }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(data);
+            liri();
+        });
+    })
+}
+function movieSearch() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "movie",
+            message: "What movie do you want to search for?"
+        }
+    ]).then(function (answer) {
+        url = "http://www.omdbapi.com/?i=tt3896198&apikey=8bbcfe06&t=" + answer.movie;
+        console.log(url);
+        request.get(url, function (error, response, body) {
+            if (error) {console.log('error:', error);} // Print the error if one occurred
+            console.log('body:', body); // Print the HTML for the Google homepage.
+            liri();
+        });
+    })
+}
+
+liri();
