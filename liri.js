c94da1c94780e1712c697f85426715473c23fb3a
Ben Pisaris <datcrisis@gmail.com>
2018-09-19 12:06:57 -0400

    fixed movieSearch() so the results are somewhat readable

--- liri.js
+++ liri.js
@@ -70,7 +70,9 @@ function movieSearch() {
         console.log(url);
         request.get(url, function (error, response, body) {
             if (error) {console.log('error:', error);} // Print the error if one occurred
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            console.log(
+                JSON.parse(body)
+            ); // Print the HTML for the Google homepage.
             liri();
         });
     })
